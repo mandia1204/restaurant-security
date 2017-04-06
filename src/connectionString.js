1ad739552324ae6156b2c99952c32c0d729ab81d
mandia1204 <mandia1204@gmail.com>
2017-04-06 17:14:06 -0500

    update docker files and package.json

--- src/connectionString.js
+++ src/connectionString.js
@@ -3,7 +3,8 @@ const getConnectionString = () => {
   const pwd = process.env.SECURITY_DB_PASSWORD;
   const port = process.env.SECURITY_DB_PORT;
   const dbname = process.env.SECURITY_DB_DATABASE;
-  return `mongodb://${uname}:${pwd}@res_security_db:${port}/${dbname}`
+  return `mongodb://${uname}:${pwd}@res_security_db:${port}/${dbname}`;
+  //return 'mongodb://localhost:27017/users';
 };
 
 export default getConnectionString;
