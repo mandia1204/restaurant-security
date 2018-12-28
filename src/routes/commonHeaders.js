8d2c54f599673380a58892a9736b51a1afa350d6
Marvin Andia <mandia1204@gmail.com>
2018-12-28 15:31:31 -0500

    adding prometheus client and a sample metric

--- src/routes/commonHeaders.js
+++ src/routes/commonHeaders.js
@@ -1,8 +1,15 @@
+import promClient from 'prom-client';
 import mongooseUtil from '../db/mongooseUtil';
 import DebugNamespaces from '../util/debugNameSpaces';
 import Logger from '../util/logger';
 
 const logger = Logger(DebugNamespaces.http);
+
+const counter = new promClient.Counter({
+  name: 'security_app_http_requests_total',
+  help: 'number of http requests',
+  labelNames: ['method', 'handler'],
+});
 const commonHeaders = (app) => {
   const db = mongooseUtil();
   app.use((req, res, next) => {
@@ -11,6 +18,7 @@ const commonHeaders = (app) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     logger.info('%s %s %o', req.method, req.url, req.headers);
+    counter.inc({ method: req.method, handler: req.url });
     next();
   });
 };
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import promClient from 'prom-client';
 import express from './expressServer';
 import commonHeaders from './routes/commonHeaders';
 import userRoutes from './routes/userRoutes';
@@ -7,10 +8,16 @@ import Logger from './util/logger';
 
 const logger = Logger(DebugNamespaces.server);
 const app = express().getServer();
+const { register } = promClient;
 
 commonHeaders(app);
 userRoutes(app);
 tokenRoutes(app);
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(register.metrics());
+});
 
 logger.info('Starting security-app version 1.3');
 
