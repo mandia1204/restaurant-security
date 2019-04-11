ccfe3857dc6ce57909d9a3dbe5d603b47903804f
Marvin Andia <mandia1204@gmail.com>
2019-04-10 20:51:20 -0500

    update scripts

--- src/db/scripts/startupScript.js
+++ src/db/scripts/startupScript.js
@@ -1,5 +1,12 @@
 db.createCollection("users");
 
-db.users.insert({userName:'matt', password:'1234'});
+db.users.insert({name:'marvin', userName:'matt', password:'1234', isAdmin:true, roles: []});
 
 db.createCollection("tokens");
+
+db.createCollection("roles");
+
+db.roles.insert({roleName:'admin'});
+db.roles.insert({roleName:'user'});
+db.roles.insert({roleName:'operator'});
+db.roles.insert({roleName:'reader'});
