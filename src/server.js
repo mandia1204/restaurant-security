8542d0978c6c44e6c2b5aea104d9e62b1d9c361c
Marvin Andia <mandia1204@gmail.com>
2020-03-24 19:45:43 -0500

    adding swagger for ambassador

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import promClient from 'prom-client';
 import './setup';
 import { initGlobalTracer } from 'opentracing';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 import config from 'config';
 import express from './expressServer';
 import commonHeaders from './routes/commonHeaders';
@@ -12,6 +14,8 @@ import Logger from './util/logger';
 import initTracer from './tracing/tracer';
 import tracingMiddleware from './tracing/tracing-middleware';
 
+const SWAGGER_PATH = `${__dirname}/swagger.yaml`;
+
 const logger = Logger(DebugNamespaces.server);
 const app = express().getServer();
 const { register } = promClient;
@@ -21,6 +25,9 @@ if (config.get('tracingEnabled')) {
   initGlobalTracer(initTracer('security-app'));
   app.use(tracingMiddleware());
 }
+
+// swagger
+app.use('/.ambassador-internal/openapi-docs', swaggerUi.serve, swaggerUi.setup(yaml.load(SWAGGER_PATH)));
 
 commonHeaders(app);
 userRoutes(app);
@@ -33,7 +40,7 @@ app.get('/metrics', (req, res) => {
 });
 
 
-logger.info('Starting security-app version 1.6');
+logger.info('Starting security-app version 1.7');
 
 const port = 3001;
 app.listen(port, (err) => {
