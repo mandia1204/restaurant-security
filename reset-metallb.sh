be16ec4aeade2df4e9ba615dc6ac951bdc64c568
Marvin Andia <mandia1204@gmail.com>
2020-04-05 11:35:23 -0500

    fixing spacing in vscode

--- reset-metallb.sh
+++ reset-metallb.sh
@@ -3,6 +3,4 @@
 cd /c/Users/Marvin/Documents/Restaurant/restaurant-security/k8s/init
 kubectl apply -f 02-metal-lb-config.yaml
 kubectl delete pods -l=app=metallb -n=metallb-system
-# kubectl get svc -n=istio-system
-
-#istioctl dashboard jaeger
+# kubectl get svc -n=ambassador
