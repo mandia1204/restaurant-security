4d217807a05fbb0c5315c1f1e1d21d76f1e87765
Marvin Andia <mandia1204@gmail.com>
2020-07-07 11:37:19 -0500

    updating start_server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,4 +7,4 @@ export SECURITY_DB_PASSWORD=1234
 export SECURITY_ACCESS_TOKEN_SECRET=mysecret123
 export SECURITY_REFRESH_TOKEN_SECRET=mysecret324
 cd /home/ec2-user/app/
-nohup npm run serve &
+nohup node securityApp.js &
