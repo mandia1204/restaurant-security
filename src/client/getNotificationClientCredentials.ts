01b68494f68bec15635d8848cde68b69ec6417da
Marvin Andia <mandia1204@gmail.com>
2022-07-01 15:58:11 -0500

    enabling notification mutual tls

--- src/client/getNotificationClientCredentials.ts
+++ src/client/getNotificationClientCredentials.ts
@@ -1,4 +1,4 @@
-// import fs from 'fs';
+import fs from 'fs';
 import grpc from 'grpc';
 import path from 'path';
 import config from 'config';
@@ -7,11 +7,11 @@ const baseDir: string = config.get('baseDir');
 
 const getNotificationClientCredentials = () => {
   const certPath = path.join(baseDir, 'cert');
-  // const caCert = fs.readFileSync(path.join(certPath, 'ca.crt'));
-  // const clientKey = fs.readFileSync(path.join(certPath, 'client.key'));
-  // const clientCert = fs.readFileSync(path.join(certPath, 'client.crt'));
+  const caCert = fs.readFileSync(path.join(certPath, 'ca.crt'));
+  const clientKey = fs.readFileSync(path.join(certPath, 'client.key'));
+  const clientCert = fs.readFileSync(path.join(certPath, 'client.crt'));
 
-  return grpc.credentials.createInsecure(); // .createSsl(caCert, clientKey, clientCert);
+  return grpc.credentials.createSsl(caCert, clientKey, clientCert);
 };
 
 export default getNotificationClientCredentials;
