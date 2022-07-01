016aed98ab4cc3d1e0d60ee7697e1d7ecdbc41ed
Marvin Andia <mandia1204@gmail.com>
2022-07-01 15:26:30 -0500

    adding log

--- src/client/notificationServiceClient.ts
+++ src/client/notificationServiceClient.ts
@@ -1,7 +1,11 @@
 import config from 'config';
 import { NotificationClient } from '../grpc-services/notification_grpc_pb';
 import getNotificationClientCredentials from './getNotificationClientCredentials';
+import Logger from '../util/logger';
+import DebugNamespaces from '../util/debugNameSpaces';
 
+const logger = Logger(DebugNamespaces.grpc);
+logger.info(`connecting to notification service, url: ${config.get('notificationUrl')}`);
 const client = new NotificationClient(config.get('notificationUrl'), getNotificationClientCredentials());
 
 export default client;
--- src/util/debugNameSpaces.js
+++ src/util/debugNameSpaces.js
@@ -3,6 +3,7 @@ export default {
   token: 'app:token',
   mongo: 'app:mongo',
   http: 'app:http',
+  grpc: 'app:grpc',
   userRoutes: 'app:userRoute',
   strategyCallback: 'app:strategyCallback',
 };
